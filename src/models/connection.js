3943162cbfd7791f1ba1ae45f8adef524eb4e4b8
GeovaneVLG <geovanevinicius2012@hotmail.com>
2023-09-30 19:56:50 -0300

    correção connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 require('dotenv').config();
 const MYSQL_HOST = process.env.MYSQL_HOST;
-const MYSQL_USER = process.env.MYSQL_HOST;
-const MYSQL_PASSWORD = process.env.MYSQL_HOST;
-const MYSQL_DB = process.env.MYSQL_HOST;
+const MYSQL_USER = process.env.MYSQL_USER;
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWOR;
+const MYSQL_DB = process.env.MYSQL_DB;
 
 const connection = mysql.createPool({
     host: MYSQL_HOST,
