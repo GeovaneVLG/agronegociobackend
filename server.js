a94ccf2586d8b7d32025fb004a7b85e3d55d36ed
GeovaneVLG <geovanevinicius2012@hotmail.com>
2023-10-03 10:47:22 -0300

    atualizacao dos arquivos server/connections/env

---
+++ server.js
@@ -0,0 +1,4 @@
+const app = require('./src/app');
+require('dotenv').config();
+
+app.listen(3000, () => console.log('server rodando'));
--- src/models/connection.js
+++ src/models/connection.js
@@ -1,13 +1,15 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
 require('dotenv').config();
 const MYSQL_HOST = process.env.MYSQL_HOST;
 const MYSQL_USER = process.env.MYSQL_USER;
 const MYSQL_PASSWORD = process.env.MYSQL_PASSWOR;
 const MYSQL_DB = process.env.MYSQL_DB;
+const MYSQL_PORT = process.env.MYSQL_PORT;
 
 const connection = mysql.createPool({
     host: MYSQL_HOST,
+    port: MYSQL_PORT,
     user: MYSQL_USER,
     password: MYSQL_PASSWORD,
     database: MYSQL_DB,
