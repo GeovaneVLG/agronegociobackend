cb791cd2f2c2379dac52f721be5804d9ac8e4096
GeovaneVLG <geovanevinicius2012@hotmail.com>
2023-10-03 10:57:52 -0300

    atualizacao do arquivo connections

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 const MYSQL_HOST = process.env.MYSQL_HOST;
 const MYSQL_USER = process.env.MYSQL_USER;
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWOR;
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD;
 const MYSQL_DB = process.env.MYSQL_DB;
 const MYSQL_PORT = process.env.MYSQL_PORT;
 
